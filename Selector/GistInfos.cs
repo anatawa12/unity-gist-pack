a497f020a7fb6b65e47547439aa3c0a647351dd9
anatawa12 <anatawa12@icloud.com>
2023-09-02 01:33:28 +0900

    chore: store gist data in json

---
+++ Selector/GistInfos.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using JetBrains.Annotations;
+using UnityEditor;
+using UnityEngine;
+
+namespace anatawa12.gists.selector
+{
+    static class GistInfoLoader
+    {
+        public static GistInfo[] Load()
+        {
+            var path = AssetDatabase.GUIDToAssetPath("0dcdca9645fe442ea8825da0c868bffa");
+            return JsonUtility.FromJson<AllGistsInfo>(File.ReadAllText(path))
+                .gists.Select(Convert).ToArray();
+        }
+
+        private static GistInfo Convert(JsonGistInfo jsonInfo) =>
+            new GistInfo(
+                jsonInfo.id ?? throw new ArgumentException("id is null"),
+                jsonInfo.name ?? throw new ArgumentException("name is null"),
+                jsonInfo.description ?? throw new ArgumentException("description is null"),
+                ConvertDefines(jsonInfo.constraints)
+            );
+
+        private static Define ConvertDefines(string[] jsonInfoConstraints) => jsonInfoConstraints.Aggregate(Define.None,
+            (current, jsonInfoConstraint) => current | Defines[jsonInfoConstraint]);
+
+        private static readonly Dictionary<string, Define> Defines = Enum.GetValues(typeof(Define))
+            .Cast<Define>()
+            .ToDictionary(x => x.ToString(), x => x);
+    }
+
+    readonly struct GistInfo
+    {
+        public readonly string ID;
+        public readonly string Name;
+        public readonly string Description;
+        public readonly Define DependencyConstants;
+
+        public GistInfo([NotNull] string id, [NotNull] string name, [NotNull] string description,
+            Define dependencyConstants = Define.None)
+        {
+            ID = id ?? throw new ArgumentNullException(nameof(id));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Description = description ?? throw new ArgumentNullException(nameof(description));
+            DependencyConstants = dependencyConstants;
+        }
+    }
+
+    [Serializable]
+    struct AllGistsInfo
+    {
+        public JsonGistInfo[] gists;
+    }
+
+    [Serializable]
+    struct JsonGistInfo
+    {
+        public string id;
+        public string name;
+        public string description;
+        public string[] constraints;
+    }
+}
--- Selector/Selector.cs
+++ Selector/Selector.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
 
@@ -15,93 +14,7 @@ namespace anatawa12.gists.selector
         const string SettingsFolderPath = "ProjectSettings/Packages/com.anatawa12.gists";
         const string GistsFilePath = SettingsFolderPath + "/EnabledGists.txt";
 
-        public static readonly GistInfo[] Gists =
-        {
-            new GistInfo("257fbbebd9b7dab8bf39c0c710a2bfc7", 
-                "CompileLogger",
-                "The .cs file to log compilation to some file. useful with tail -f"),
-
-            new GistInfo("def5f8a29179ecbcb45502fa3b4590ce", 
-                "CreateAssemblyDefinitionForUdonSharp",
-                "Adds menu to create assembly definition with U# assembly definition",
-                Define.UDON_SHARP),
-
-            new GistInfo("e825ec4ee39ae29b64fdcc2f3f07a58c", 
-            "MapGameObjects",
-                "MapGameObjects"),
-
-            new GistInfo("4476430cfcc2ccef4bc40341d20001cf", 
-                "Fake DynamicBone Components",
-                "Fake DynamicBone Component to make DB -> PB Converter in VRChat SDK works."),
-
-            new GistInfo("a4bb4e2e5d75b4fa5ba42e236aae564d", 
-            "ActualPerformanceWindow",
-                "A window to see actual performance rank on building avatars",
-                Define.VRCSDK_AVATARS),
-
-            new GistInfo("379c4d828c2a0add4d623f8668209cbc", 
-            "PhysBoneEditorUtilities",
-                "Set of utilities for PhysBone", 
-            Define.VRCSDK_AVATARS),
-
-            new GistInfo("4c900d5c15050fb5bdc0f9d027962183", 
-            "GenerateMeshWithBackFace",
-                "generate new mesh with backface. useful for quest avatars"),
-
-            new GistInfo("b8799da5d3131e4020f414439a4ea037", 
-            "Transfer Transform Window",
-                "A window to copy transform info recursively"),
-
-            new GistInfo("581b66619711eaf5ebacbd85369d62e6", 
-            "SetRandomBlueprintId",
-                "Set blueprint id without build",
-                Define.VRCSDK_BASE),
-
-            new GistInfo("5f847a1692fb30c2c9f00a47d50243ad", 
-                "AndroidOnlyCheck",
-                "A VRCSDKPreprocessAvatarCallback which prevents PC builds.",
-                Define.VRCSDK_AVATARS),
-
-            new GistInfo("f7476d2d727bc43d86121f6a3337d2c3", 
-                "MergeAnimationClip",
-                "Micro tool to multiple animation clip into one."),
-
-            new GistInfo("5987f6b5357c3c91603fa07f215dfeab", 
-            "CompilationLogWindow",
-                "The window to see compilation progress"),
-
-            new GistInfo("8375f82dbc751086a32fcd2c626fa09b",
-                "HumanoidInfoWindow",
-                "Actual bone mapping to Animator"),
-
-            new GistInfo("875d0776305b771ba7ee74c656f082f6",
-                "EditorScreenshotTakeTool",
-                "A tool to take screen shot of an Editor"),
-
-            new GistInfo("94d6fd4272025fd26962476100a20ff0",
-                "SelectSkinnedMeshBones",
-                "Select GameObjects used by SkinnedMeshRenderers"),
-
-            new GistInfo("ae5f7b3c5e07150ddc1eb9f0948019ff", 
-                "FindReferenceChainRecursive",
-                "Tool to find unexpected references in avatars or scenes"),
-
-            new GistInfo("930c08c724af17197a401bcfd580985b",
-                "RemovePropertiesFromAnimations",
-                "A window to remove some property from multiple animations"),
-
-            new GistInfo("ecf33339c315f259cee62b304910fe43",
-                "SetDirtyRecursively",
-                "Set dirty all components on selected GameObject to avoid reference to prefab asset"),
-
-            new GistInfo("4733d6e695df5dd5a08c599189bba589",
-                "AlignXAxisOnPlaneWithYRotation",
-                "Align X axis of multiple GameObjects onto plane of the gameobjects. This is made to align rotation of skirt bone."),
-
-            new GistInfo("6af2d5e41b0941aee1ed3fd7e0860ac0",
-                "MultiGizmo",
-                "Align X axis of multiple GameObjects onto plane of the gameobjects. This is made to align rotation of skirt bone."),
-        };
+        public static readonly GistInfo[] Gists = GistInfoLoader.Load();
 
         public static readonly IReadOnlyDictionary<string, GistInfo> GistsById = Gists.ToDictionary(x => x.ID);
 
@@ -185,23 +98,6 @@ namespace anatawa12.gists.selector
             foreach (var s in config)
                 result.Append(s).Append('\n');
             File.WriteAllText(GistsFilePath, result.ToString());
-        }
-    }
-
-    readonly struct GistInfo
-    {
-        public readonly string ID;
-        public readonly string Name;
-        public readonly string Description;
-        public readonly Define DependencyConstants;
-
-        public GistInfo([NotNull] string id, [NotNull] string name, [NotNull] string description,
-            Define dependencyConstants = Define.None)
-        {
-            ID = id ?? throw new ArgumentNullException(nameof(id));
-            Name = name ?? throw new ArgumentNullException(nameof(name));
-            Description = description ?? throw new ArgumentNullException(nameof(description));
-            DependencyConstants = dependencyConstants;
         }
     }
 
